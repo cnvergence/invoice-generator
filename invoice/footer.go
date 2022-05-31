f26fc6929274062ce67f1f7fef9bbb3294f905b2
Karol Szwaj <karol.szwaj@gmail.com>
2022-05-31 17:44:55 +0200

    Rework footer

--- invoice/footer.go
+++ invoice/footer.go
@@ -1,10 +1,7 @@
 package invoice
 
 import (
-	"encoding/base64"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"strconv"
 
 	"github.com/johnfercher/maroto/pkg/consts"
@@ -16,33 +13,6 @@ func (i *Invoice) buildFooter() {
 	i.pdf.RegisterFooter(func() {
 		i.pdf.SetAliasNbPages("{nbs}")
 		currentPage := strconv.Itoa(i.pdf.GetCurrentPage())
-		base64StringImage, err := loadImage("img/go.png")
-		if err != nil {
-			fmt.Println("Could not load image:", err)
-			os.Exit(1)
-		}
-		i.pdf.Row(6, func() {
-			i.pdf.Col(12, func() {
-				i.pdf.Text("Generated with", props.Text{
-					Top:   1,
-					Style: consts.BoldItalic,
-					Size:  8,
-					Align: consts.Left,
-					Color: getTealColor(),
-				})
-
-			})
-		})
-		i.pdf.Row(8, func() {
-			i.pdf.Col(15, func() {
-				_ = i.pdf.Base64Image(base64StringImage, consts.Png, props.Rect{
-					Left:    5,
-					Top:     1,
-					Center:  false,
-					Percent: 100,
-				})
-			})
-		})
 		i.pdf.Row(6, func() {
 			i.pdf.Col(12, func() {
 				i.pdf.Text(fmt.Sprintf("Page %s of {nbs}", currentPage), props.Text{
@@ -54,15 +24,16 @@ func (i *Invoice) buildFooter() {
 				})
 			})
 		})
+		i.pdf.Row(6, func() {
+			i.pdf.Col(12, func() {
+				i.pdf.Text("github.com/cnvergence/invoice-generator", props.Text{
+					Top:   1,
+					Style: consts.BoldItalic,
+					Size:  8,
+					Align: consts.Left,
+					Color: getTealColor(),
+				})
+			})
+		})
 	})
-
-}
-
-func loadImage(path string) (string, error) {
-	img, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-
-	return base64.StdEncoding.EncodeToString(img), nil
 }
